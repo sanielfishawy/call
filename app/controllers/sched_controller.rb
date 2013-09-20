5046264a66bc70fb0a153cbe44d77d209daba549
Sani Elfishawy <sani@sbcglobal.net>
2013-09-20 13:05:35 -0700

    Sort players by first name in sched view

--- app/controllers/sched_controller.rb
+++ app/controllers/sched_controller.rb
@@ -2,7 +2,7 @@ class SchedController < ApplicationController
   
   def sched
     @players = Player.all
-    @cast_and_crew = {:crew => Player.crew, :cast => Player.cast}
+    @cast_and_crew = {:crew => Player.crew.sort{|a,b| a.first_name <=> b.first_name}, :cast => Player.cast.sort{|a,b| a.first_name <=> b.first_name}}
   end
   
 end
